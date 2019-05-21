a1738077092ce2d977833c17fa6470cf3758a29b
Steffen Meinecke <steffen.meinecke@uni-kassel.de>
2019-05-21 13:04:28 +0200

    add matplotlib to MOCK_MODULES

--- doc/conf.py
+++ doc/conf.py
@@ -13,12 +13,16 @@
 
 import os
 import sys
+import mock
+
+MOCK_MODULES = ['matplotlib', 'matplotlib.pyplot']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-autodoc_mock_imports = ['_tkinter']
 
 # -- General configuration -----------------------------------------------------
 
